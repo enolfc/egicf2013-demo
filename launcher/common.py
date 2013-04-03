66efe91aa01710bb2e5fab66fe96177ddf16fed9
Enol Fernandez <enolfc@ifca.unican.es>
2013-04-03 16:50:17 +0200

    New scripts for the demo.

---
+++ launcher/common.py
@@ -0,0 +1,41 @@
+#
+# some common code...
+#
+
+import json
+import requests
+import subprocess
+import sys
+
+
+def get_vo_info():
+    # get VO information
+    try:
+        vo_info = subprocess.check_output('voms-proxy-info -vo -path',
+                                          shell=True,
+                                          stderr=subprocess.STDOUT)
+        proxy, vo_name = vo_info.split()
+    except subprocess.CalledProcessError:
+        print >> sys.stderr, "Not able to get proxy info..."
+        sys.exit(1)
+    return proxy, vo_name
+
+
+def keystone_auth(ks_uri, proxy, vo_name):
+    print "Authenticating against %s" % ks_uri
+    r = requests.post(ks_uri + 'v2.0/tokens',
+                      headers={'content-type': 'application/json',
+                               'accept': 'application/json', },
+                      data=json.dumps({'auth': {'tenantName': vo_name,
+                                                'voms': True}
+                                       }),
+                      cert=proxy,
+                      verify=False)
+    if r.status_code != 200:
+        print >> sys.stderr, "Something went wrong, out!"
+        sys.exit(1)
+    try:
+        return r.json()['access']['token']['id']
+    except KeyError:
+        print >> sys.stderr, "Something went wrong, out!"
+        sys.exit(1)
